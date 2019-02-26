3164677d1d030ac5c4d11725763e2927433d03df
Ralf Pinheiro <pinheiro.ralf@gmail.com>
2019-02-26 22:18:13 +0000

    Creates a findPerson function using Knex in knex_example file

--- knex_example.js
+++ knex_example.js
@@ -1,15 +1,44 @@
-const pg = require("pg");
+// const pg = require("pg");
 const settings = require("./settings"); // settings.json
 const value = process.argv[2];
 
 var knex = require("knex")({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    user: "settings.user",
-    password: "settings.password",
-    database: "settings.database"
+    host: settings.hostname,
+    user: settings.user,
+    password: settings.password,
+    database: settings.database
   }
 });
 
-console.log(knex.from("famous_people").select("first_name", "last_name", "birthdate"));
+function errorHandling(err) {
+  if (err) {
+    return console.error("Error:", err);
+  }
+}
+
+function findPeople(name) {
+  knex
+    .select("*")
+    .from("famous_people")
+    .where("first_name", "=", name)
+    .asCallback(function(err, rows) {
+      //   errorHandling(err);
+      console.log(`Found ${rows.length} person(s) by the name '${name}'`);
+
+      for (let name in rows) {
+        let count = Number(name) + 1;
+        console.log(
+          `- ${count} ${rows[name].first_name} ${rows[name].last_name}, born ${rows[name].birthdate
+            .toString()
+            .slice(0, 15)}`
+        );
+      }
+    })
+    .finally(function() {
+      knex.destroy();
+    });
+}
+
+findPeople(value);
